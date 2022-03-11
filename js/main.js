0564f3f1e632ffc198fdfd7494ea251306ed244b
Jamoliddin610 <najmiddinovjamoliddin571@gmail.com>
2022-03-11 06:31:52 +0500

    jamoliddin finished img

---
+++ js/main.js
@@ -0,0 +1,43 @@
+let imgs = document.getElementById('imgs');
+let leftBtn = document.getElementById('left');
+let rightBtn = document.getElementById('right');
+
+
+let img = document.querySelectorAll('#imgs img');
+
+let idx = 0;
+
+function changeImg() {
+    if (idx > img.length - 1) {
+        idx = 0
+    } else if (idx < 0) {
+        idx = img.length - 1
+    }
+    imgs.style.transform = `translateX(${-idx * 500}px)`
+}
+
+rightBtn.addEventListener('click', function change() {
+    idx++;
+    resetInter();
+    changeImg()
+})
+leftBtn.addEventListener('click', function change() {
+    idx--;
+    resetInter();
+    changeImg()
+})
+
+let interval = setInterval(run, 2000);
+
+function run() {
+    idx++;
+
+    changeImg();
+}
+
+function resetInter() {
+    clearInterval(interval);
+
+     interval = setInterval(run, 2000);
+
+}
